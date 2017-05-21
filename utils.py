fe3432d9860eb9a0564982b114f5997c82a69b8e
joneswack <jonas.wacker@gmail.com>
2017-05-21 06:10:23 -0300

    added .gitignore and cifar dataset

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import struct
 from array import array as pyarray
+import pickle
+import tarfile
 
 def load_mnist(dataset="training", digits=np.arange(10), path=".", size = 60000):
     if dataset == "training":
@@ -34,3 +36,41 @@ def load_mnist(dataset="training", digits=np.arange(10), path=".", size = 60000)
         labels[i] = lbl[ind[i]]
     labels = [label[0] for label in labels]
     return images, labels
+
+def convert_image_from_pack(raw_data):
+    images = []
+    labels = []
+    
+    def get_image_format(data):
+        return np.array([[data[i], data[i + 1024], data[i + 2048]] for i in range(1024)])
+
+    for i in range(len(raw_data[b'data'])):
+        image_format = get_image_format(raw_data[b'data'][i])
+        label = raw_data[b'labels'][i]
+        #filepath = path + "/" + str(label) + "/" + raw_data['filenames'][i]
+        #saveImage(image_format, "RGB", (32, 32), filepath)
+        #logfile.write(filepath + "\t" + str(label) + "\n")
+        images += [image_format]
+        labels += [label]
+        
+    return np.array(images), labels
+
+def load_cifar10(filename):
+    batches = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', 'test_batch']
+    
+    tar = tarfile.open(filename, "r:gz")
+    
+    tar_contents = [(file.name.split('/')[-1], file) for file in tar.getmembers()]
+    tar_contents = sorted(filter(lambda x: x[0] in batches, tar_contents), key=lambda x: x[0])
+    all_images = {}
+    all_labels = {}
+
+    for member in tar_contents:
+        f = tar.extractfile(member[1])
+        if f is not None:
+            content = pickle.load(f, encoding='bytes')
+            images, labels = convert_image_from_pack(content)
+            all_images[member[0]] = images
+            all_labels[member[0]] = labels
+    tar.close()
+    return all_images, all_labels
